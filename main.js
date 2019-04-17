7b609f9b39c66c5202aab6c41f88f6ee8f4b4ecd
Mintae Lee <lee.mintae@codeimmersives.com>
2019-04-17 14:16:52 -0400

    created toggle for theme change

--- main.js
+++ main.js
@@ -1,29 +1,53 @@
 window.onload = init;
 
 function init(){
-    document.querySelector("#toggle-theme")
+    document.querySelector(".toggle-theme")
         .addEventListener('click', toggleTheme);
 
-    document.querySelector("#days-left").innerText = countdown();
+    document.querySelector(".days-left").innerText = countdown();
         
 }
 
 function toggleTheme(){
     event.preventDefault();
 
-    const app = document.querySelector('.app');
-    const button = event.target;
+    const away = document.querySelector('.away');
+    const home = document.querySelector('.home');
+    const awayName = document.querySelector('.away-name');
+    const homeName = document.querySelector('.home-name');
+    const awayLogo = document.querySelector('.away-logo');
+    const homeLogo = document.querySelector('.home-logo');
+    const stadium = document.querySelector('.stadium');
+    const city = document.querySelector('.city');
+    const button = document.querySelector('.toggle-theme');
 
-    app.classList.toggle('flat-theme');
+    away.classList.toggle('warriors');
+    home.classList.toggle('bucks');
+
     
-    button.innerText = button.innerText === `Switch to regular theme`
-        ? `Switch to flat theme`
-        : `Switch to regular theme`;
+    if (button.innerText === `Switch to Warriors Home Game`) {
+        button.innerText = `Switch to Bucks Home Game`;
+        awayName.innerText = `Milwaukee Bucks`;
+        homeName.innerText = `Golden State Warriors`;
+        awayLogo.src = './assets/buckslogo.png'
+        homeLogo.src = './assets/warriorslogo.png'
+        stadium.innerText = 'Oracle Arena'
+        city.innerText = 'Oakland, CA'
+    } else {
+        button.innerText = `Switch to Warriors Home Game`;
+        awayName.innerText = `Golden State Warriors`;
+        homeName.innerText = `Milwaukee Bucks`;
+        awayLogo.src = './assets/warriorslogo.png'
+        homeLogo.src = './assets/buckslogo.png'
+        stadium.innerText = 'Fiserv Forum'
+        city.innerText = 'Milwaukee, WI'
+    }
 }
 
 function countdown(){
     let currentdate =new Date();
 
+    document.querySelector(".date").innerText = dateText(currentdate);
     let gameday = new Date("May 30, 2019");
     let timeleft = gameday - currentdate;
     let msPerDay = 24 * 60 * 60 * 1000;
@@ -35,3 +59,15 @@ function countdown(){
     return text;
 }
 
+
+function dateText(date){
+    let monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    let month = monthList[date.getMonth()];
+    let day = date.getDate();
+    let year = date.getFullYear();
+
+    return `${month} ${day}, ${year}`
+
+
+}
