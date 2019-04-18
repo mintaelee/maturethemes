ac4800114db3cb45220143861f4457e53c889ce9
Mintae Lee <lee.mintae@codeimmersives.com>
2019-04-17 21:27:15 -0400

    added dark theme'

---
+++ data.js
@@ -0,0 +1,125 @@
+const teams = [
+    {
+        "name": "Golden State Warriors",
+        "abName": "Warriors",
+        "home-color": "rgb(253,185,39)",
+        "away-color": "rgb(38,40,42)",
+        "stadium": "Oracle Arena",
+        "city": "Oakland, CA"
+    },
+
+    {
+        "name": "Milwaukee Bucks",
+        "abName": "Bucks",
+        "home-color": "rgb(240,235,210)",
+        "away-color": "rgb(0,71,27)",
+        "stadium": "Fiserv Forum",
+        "city": "Milwaukee, WI"
+    },
+
+    {
+        "name": "Los Angeles Clippers",
+        "abName": "Clippers",
+        "home-color": "rgb(200,16,46)",
+        "away-color": "rgb(190,192,194)",
+        "stadium": "Staples Center",
+        "city": "Los Angeles, CA"
+    },
+
+    {
+        "name": "Houston Rockets",
+        "abName": "Rockets",
+        "home-color": "rgb(206,17,65)",
+        "away-color": "rgb(196,206,211)",
+        "stadium": "Toyota Center",
+        "city": "Houston, TX"
+    },
+
+    {
+        "name": "Utah Jazz",
+        "abName": "Jazz",
+        "home-color": "rgb(0,43,92)",
+        "away-color": "rgb(249,160,27)",
+        "stadium": "Vivint Smart Home Arena",
+        "city": "Salt Lake City, UT"
+    },
+
+    {
+        "name": "Portland Trail Blazers",
+        "abName": "Blazers",
+        "home-color": "rgb(224,58,62)",
+        "away-color": "rgb(6,25,34)",
+        "stadium": "Moda Center",
+        "city": "Portland, OR"
+    },
+
+    {
+        "name": "Oklahoma City Thunder",
+        "abName": "Thunder",
+        "home-color": "rgb(0,125,195)",
+        "away-color": "rgb(239,59,36)",
+        "stadium": "Chesapeake Energy Arena",
+        "city": "Oklahoma City, OK"
+    },
+
+    {
+        "name": "Denver Nuggets",
+        "abName": "Nuggets",
+        "home-color": "rgb(13,34,64)",
+        "away-color": "rgb(255,198,39)",
+        "stadium": "Pepsi Center",
+        "city": "Denver, CO"
+    },
+
+    {
+        "name": "San Antonio Spurs",
+        "abName": "Spurs",
+        "home-color": "rgb(196,206,211)",
+        "away-color": "rgb(6,25,34)",
+        "stadium": "AT&T Center",
+        "city": "San Antonio, TX"
+    },
+
+
+
+
+]
+
+const firstRound = [
+    {
+        "match": "Clippers vs Warriors",
+        "away": "Los Angeles Clippers",
+        "home": "Golden State Warriors"
+    },
+
+    {
+        "match": "Jazz vs Rockets",
+        "away": "Utah Jazz",
+        "home": "Houston Rockets"
+    },
+    
+    {
+        "match": "Thunder vs Blazers",
+        "away": "Oklahoma City Thunder",
+        "home": "Portland Trail Blazers"
+    },
+
+    {
+        "match": "Spurs vs Nuggets",
+        "away": "San Antonio Spurs",
+        "home": "Denver Nuggets"
+    }
+    
+
+]
+
+const finalRound = [
+    {
+        "match": "Warriors vs Bucks",
+        "away": "Golden State Warriors",
+        "home": "Milwaukee Bucks"
+    }
+    
+]
+
+
--- main.js
+++ main.js
@@ -1,47 +1,132 @@
 window.onload = init;
 
 function init(){
+
+    setDefault();
+
+    document.querySelector(".toggle-home")
+        .addEventListener('click', toggleHome);
+
     document.querySelector(".toggle-theme")
         .addEventListener('click', toggleTheme);
 
     document.querySelector(".days-left").innerText = countdown();
+
+    document.querySelector(".bracket-select")
+        .addEventListener('change', toggleMatch);
+
+    document.querySelector(".match-select")
+        .addEventListener('change', toggleTeams);
+
         
+}
+
+function setDefault(){
+    toggleTheme();
+    changeAway('golden state warriors');
+    changeHome('milwaukee bucks');
+
+}
+
+function toggleMatch(){
+    event.preventDefault();
+
+    let round = finalRound;
+
+    const bracketSelected = document.querySelector('.bracket-select').value;
+    const matchDropDown = document.querySelector('.match-select')
+
+
+    if(bracketSelected === 'first-round'){
+        round = firstRound;
+    } else {
+        round = finalRound;
+    }
+
+    clearDropDown();
+    for (let i = 0; i < round.length; i++){
+        let matchup = document.createElement('option');
+        matchup.text = matchup.value = round[i].match;
+        matchDropDown.add(matchup, 0);
+    }
+
+}
+
+function toggleTeams(){
+    event.preventDefault();
+    const matchSelected = document.querySelector('.match-select').value;
+    const bracketSelected = document.querySelector('.bracket-select').value;
+
+    if(bracketSelected === 'first-round'){
+        round = firstRound;
+    } else {
+        round = finalRound;
+    }
+
+    let index = searchMatchIndex(matchSelected, round);
+
+    changeAway(round[index].away);
+    changeHome(round[index].home);
+
+
+}
+
+function toggleHome(){
+    event.preventDefault();
+
+    let currentAway = document.querySelector('.away-name').innerText;
+    let currentHome = document.querySelector('.home-name').innerText;
+
+    changeAway(currentHome);
+    changeHome(currentAway);
+
+    // const away = document.querySelector('.away');
+    // const home = document.querySelector('.home');
+    // const awayName = document.querySelector('.away-name');
+    // const homeName = document.querySelector('.home-name');
+    // const awayLogo = document.querySelector('.away-logo');
+    // const homeLogo = document.querySelector('.home-logo');
+    // const stadium = document.querySelector('.stadium');
+    // const city = document.querySelector('.city');
+    // const button = document.querySelector('.toggle-home');
+
+    // away.classList.toggle('warriors');
+    // home.classList.toggle('bucks');
+
+    
+    // if (button.innerText === `Switch to Warriors Home Game`) {
+    //     button.innerText = `Switch to Bucks Home Game`;
+    //     awayName.innerText = `Milwaukee Bucks`;
+    //     homeName.innerText = `Golden State Warriors`;
+    //     awayLogo.src = './assets/buckslogo.png'
+    //     homeLogo.src = './assets/warriorslogo.png'
+    //     stadium.innerText = 'Oracle Arena'
+    //     city.innerText = 'Oakland, CA'
+    // } else {
+    //     button.innerText = `Switch to Warriors Home Game`;
+    //     awayName.innerText = `Golden State Warriors`;
+    //     homeName.innerText = `Milwaukee Bucks`;
+    //     awayLogo.src = './assets/warriorslogo.png'
+    //     homeLogo.src = './assets/buckslogo.png'
+    //     stadium.innerText = 'Fiserv Forum'
+    //     city.innerText = 'Milwaukee, WI'
+    // }
 }
 
 function toggleTheme(){
     event.preventDefault();
 
-    const away = document.querySelector('.away');
-    const home = document.querySelector('.home');
-    const awayName = document.querySelector('.away-name');
-    const homeName = document.querySelector('.home-name');
-    const awayLogo = document.querySelector('.away-logo');
-    const homeLogo = document.querySelector('.home-logo');
-    const stadium = document.querySelector('.stadium');
-    const city = document.querySelector('.city');
+    const darkThemes = document.querySelectorAll(".theme-toggle");
     const button = document.querySelector('.toggle-theme');
 
-    away.classList.toggle('warriors');
-    home.classList.toggle('bucks');
-
-    
-    if (button.innerText === `Switch to Warriors Home Game`) {
-        button.innerText = `Switch to Bucks Home Game`;
-        awayName.innerText = `Milwaukee Bucks`;
-        homeName.innerText = `Golden State Warriors`;
-        awayLogo.src = './assets/buckslogo.png'
-        homeLogo.src = './assets/warriorslogo.png'
-        stadium.innerText = 'Oracle Arena'
-        city.innerText = 'Oakland, CA'
-    } else {
-        button.innerText = `Switch to Warriors Home Game`;
-        awayName.innerText = `Golden State Warriors`;
-        homeName.innerText = `Milwaukee Bucks`;
-        awayLogo.src = './assets/warriorslogo.png'
-        homeLogo.src = './assets/buckslogo.png'
-        stadium.innerText = 'Fiserv Forum'
-        city.innerText = 'Milwaukee, WI'
+    for (let i = 0; i < darkThemes.length; i ++){
+        darkThemes[i].classList.toggle('dark-theme');
     }
+
+    button.innerText = button.innerText === `Switch to Dark Theme`
+        ? `Switch to Flat Theme`
+        : `Switch to Dark Theme`;
+
 }
 
 function countdown(){
@@ -70,4 +155,67 @@ function dateText(date){
     return `${month} ${day}, ${year}`
 
 
+}
+
+function clearDropDown() {
+    const dropdown = document.querySelector('.match-select');
+
+    while(dropdown.hasChildNodes()){
+        dropdown.removeChild(dropdown.firstChild);
+    }
+    let defaultDrop = document.createElement('option');
+    defaultDrop.text = defaultDrop.value = 'Select Match';
+    dropdown.add(defaultDrop, 0);
+    
+
+    
+}
+
+function changeAway(team){
+    const awayName = document.querySelector('.away-name');
+    const awayLogo = document.querySelector('.away-logo');
+    const awayDiv = document.querySelector('.away')
+    const button = document.querySelector('.toggle-home');
+
+    let index = searchTeamIndex(team);
+    awayName.innerText = teams[index].name;
+    awayLogo.src = `./assets/${teams[index].abName.toLowerCase()}logo.png`;
+    awayDiv.style.backgroundColor = teams[index]['away-color'];
+    button.innerText = `Switch to ${teams[index].abName} Home Game`;
+
+
+}
+
+function changeHome(team){
+    const homeName = document.querySelector('.home-name');
+    const homeLogo = document.querySelector('.home-logo');
+    const homeDiv = document.querySelector('.home')
+
+    const stadium = document.querySelector('.stadium');
+    const city = document.querySelector('.city');
+
+    let index = searchTeamIndex(team);
+    homeName.innerText = teams[index].name;
+    homeLogo.src = `./assets/${teams[index].abName.toLowerCase()}logo.png`;
+    stadium.innerText = teams[index].stadium;
+    city.innerText = teams[index].city;
+    homeDiv.style.backgroundColor = teams[index]['home-color'];
+
+
+}
+
+function searchTeamIndex(team){
+    for (let i = 0; i < teams.length; i++){
+        if (teams[i].name.toLowerCase() === team.toLowerCase()){
+            return i;
+        }
+    }
+}
+
+function searchMatchIndex(match,round){
+    for (let i = 0; i < round.length; i++){
+        if (round[i].match.toLowerCase() === match.toLowerCase()){
+            return i;
+        }
+    }
 }
