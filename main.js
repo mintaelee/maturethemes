be3ebea486c9c5a079a1c1e2b65eceade318a9a8
Mintae Lee <lee.mintae@codeimmersives.com>
2019-04-17 21:52:44 -0400

    added comments and cleaned up code

--- main.js
+++ main.js
@@ -2,48 +2,53 @@ window.onload = init;
 
 function init(){
 
+    // Set default page on load
     setDefault();
 
+    // Catch user clicks
     document.querySelector(".toggle-home")
         .addEventListener('click', toggleHome);
 
     document.querySelector(".toggle-theme")
         .addEventListener('click', toggleTheme);
 
-    document.querySelector(".days-left").innerText = countdown();
-
+        
     document.querySelector(".bracket-select")
-        .addEventListener('change', toggleMatch);
-
+        .addEventListener('change', switchBracket);
+        
     document.querySelector(".match-select")
-        .addEventListener('change', toggleTeams);
-
+        .addEventListener('change', switchMatch);
+    
+    // Create countdown for final game
+    document.querySelector(".days-left").innerText = countdown();
         
 }
 
+// Function to set default page on load
 function setDefault(){
+    // Start page as flat theme
     toggleTheme();
+
+    // Set default teams as warriors and bucks
     changeAway('golden state warriors');
     changeHome('milwaukee bucks');
 
 }
 
-function toggleMatch(){
+// Function to switch drop down menu for match selection based on user selected 
+// playoff bracket
+function switchBracket(){
     event.preventDefault();
 
-    let round = finalRound;
-
-    const bracketSelected = document.querySelector('.bracket-select').value;
+    let round = selectRound();
+    
     const matchDropDown = document.querySelector('.match-select')
+    
 
-
-    if(bracketSelected === 'first-round'){
-        round = firstRound;
-    } else {
-        round = finalRound;
-    }
-
+    // Clear drop down menu first
     clearDropDown();
+
+    // Fill drop down menu
     for (let i = 0; i < round.length; i++){
         let matchup = document.createElement('option');
         matchup.text = matchup.value = round[i].match;
@@ -52,25 +57,25 @@ function toggleMatch(){
 
 }
 
-function toggleTeams(){
+// Function to switch to user selected match
+function switchMatch(){
     event.preventDefault();
-    const matchSelected = document.querySelector('.match-select').value;
-    const bracketSelected = document.querySelector('.bracket-select').value;
 
-    if(bracketSelected === 'first-round'){
-        round = firstRound;
-    } else {
-        round = finalRound;
-    }
+    // Grab what the user selected
+    const matchSelected = document.querySelector('.match-select').value;
+    
+    // Grab match data
+    let round = selectRound();
 
     let index = searchMatchIndex(matchSelected, round);
 
+    // Change away and home teams
     changeAway(round[index].away);
     changeHome(round[index].home);
 
-
 }
 
+// Function to toggle home team
 function toggleHome(){
     event.preventDefault();
 
@@ -79,60 +84,37 @@ function toggleHome(){
 
     changeAway(currentHome);
     changeHome(currentAway);
-
-    // const away = document.querySelector('.away');
-    // const home = document.querySelector('.home');
-    // const awayName = document.querySelector('.away-name');
-    // const homeName = document.querySelector('.home-name');
-    // const awayLogo = document.querySelector('.away-logo');
-    // const homeLogo = document.querySelector('.home-logo');
-    // const stadium = document.querySelector('.stadium');
-    // const city = document.querySelector('.city');
-    // const button = document.querySelector('.toggle-home');
-
-    // away.classList.toggle('warriors');
-    // home.classList.toggle('bucks');
-
-    
-    // if (button.innerText === `Switch to Warriors Home Game`) {
-    //     button.innerText = `Switch to Bucks Home Game`;
-    //     awayName.innerText = `Milwaukee Bucks`;
-    //     homeName.innerText = `Golden State Warriors`;
-    //     awayLogo.src = './assets/buckslogo.png'
-    //     homeLogo.src = './assets/warriorslogo.png'
-    //     stadium.innerText = 'Oracle Arena'
-    //     city.innerText = 'Oakland, CA'
-    // } else {
-    //     button.innerText = `Switch to Warriors Home Game`;
-    //     awayName.innerText = `Golden State Warriors`;
-    //     homeName.innerText = `Milwaukee Bucks`;
-    //     awayLogo.src = './assets/warriorslogo.png'
-    //     homeLogo.src = './assets/buckslogo.png'
-    //     stadium.innerText = 'Fiserv Forum'
-    //     city.innerText = 'Milwaukee, WI'
-    // }
 }
 
+// Function to toggle theme
 function toggleTheme(){
     event.preventDefault();
 
+    // Grab all nodes that needs to be changed
     const darkThemes = document.querySelectorAll(".theme-toggle");
     const button = document.querySelector('.toggle-theme');
 
+    // Toggle on/off dark-theme
     for (let i = 0; i < darkThemes.length; i ++){
         darkThemes[i].classList.toggle('dark-theme');
     }
 
+    // Switch button text
     button.innerText = button.innerText === `Switch to Dark Theme`
         ? `Switch to Flat Theme`
         : `Switch to Dark Theme`;
 
 }
 
+// Function to create days remaining
 function countdown(){
+    // Get current date
     let currentdate =new Date();
 
+    // Display current date
     document.querySelector(".date").innerText = dateText(currentdate);
+
+    // Get game date and calculate days left  
     let gameday = new Date("May 30, 2019");
     let timeleft = gameday - currentdate;
     let msPerDay = 24 * 60 * 60 * 1000;
@@ -144,7 +126,7 @@ function countdown(){
     return text;
 }
 
-
+// Helper function to display date on more readable format
 function dateText(date){
     let monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
@@ -157,6 +139,7 @@ function dateText(date){
 
 }
 
+// Helper function to clear dropdown
 function clearDropDown() {
     const dropdown = document.querySelector('.match-select');
 
@@ -166,11 +149,9 @@ function clearDropDown() {
     let defaultDrop = document.createElement('option');
     defaultDrop.text = defaultDrop.value = 'Select Match';
     dropdown.add(defaultDrop, 0);
-    
-
-    
 }
 
+// Function to change HTML data that corresponds to selected away team
 function changeAway(team){
     const awayName = document.querySelector('.away-name');
     const awayLogo = document.querySelector('.away-logo');
@@ -186,6 +167,7 @@ function changeAway(team){
 
 }
 
+// Function to change HTML data that corresponds to selected home team
 function changeHome(team){
     const homeName = document.querySelector('.home-name');
     const homeLogo = document.querySelector('.home-logo');
@@ -204,6 +186,7 @@ function changeHome(team){
 
 }
 
+// Helper function to search index from the teams data
 function searchTeamIndex(team){
     for (let i = 0; i < teams.length; i++){
         if (teams[i].name.toLowerCase() === team.toLowerCase()){
@@ -212,10 +195,22 @@ function searchTeamIndex(team){
     }
 }
 
+// Helper function to search index from the match data
 function searchMatchIndex(match,round){
     for (let i = 0; i < round.length; i++){
         if (round[i].match.toLowerCase() === match.toLowerCase()){
             return i;
         }
     }
+}
+// Helper function to select correct data based on user selected bracket
+function selectRound(){
+    const bracketSelected = document.querySelector('.bracket-select').value;
+
+    if(bracketSelected === 'first-round'){
+        return firstRound;
+    } else {
+        return finalRound;
+    }
+    
 }
